29f6e75ca5e6dca9582e6d3739a8043c56f0a888
Nikhil Bhat <nikhilbhat021@gmail.com>
2024-11-06 17:27:16 +0530

    Setting up React

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//<img src={reactLogo} className="logo react" alt="React logo" />
+//import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (<>
+    App.jsx
+  </>)
+}
+
+export default App
